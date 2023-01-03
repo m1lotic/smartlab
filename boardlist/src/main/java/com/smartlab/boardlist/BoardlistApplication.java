d6b5785d33b30c648eb50629ae1525dc56bbc636
m1otic <parkminook16@naver.com>
2023-01-03 09:40:18 +0900

    d

---
+++ boardlist/src/main/java/com/smartlab/boardlist/BoardlistApplication.java
@@ -0,0 +1,13 @@
+package com.smartlab.boardlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardlistApplication.class, args);
+	}
+
+}
